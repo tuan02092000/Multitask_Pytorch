4214f97d0eab61612230dec45e51e40a25ea5555
TuanNV <nguyentuanbarca10@gmail.com>
2022-04-08 21:32:02 +0700

    week_2

--- model.py
+++ model.py
@@ -6,32 +6,86 @@ class MultiOutputModel(nn.Module):
         super(MultiOutputModel, self).__init__()
         self.base_model = self.model_core()
         self.x1 = nn.Linear(1000, 512)
-        self.bn = nn.BatchNorm1d(512)
-
         # Type
         self.y1 = nn.Sequential(
             nn.Linear(512, 256),
+            nn.Dropout(p=0.2, inplace=True),
             nn.ReLU(),
-            nn.BatchNorm1d(256),
-            nn.Dropout(p=0.4),
             nn.Linear(256, num_type)
         )
         self.y2 = nn.Sequential(
             nn.Linear(512, 256),
+            nn.Dropout(p=0.2, inplace=True),
             nn.ReLU(),
-            nn.BatchNorm1d(256),
-            nn.Dropout(p=0.4),
-            nn.Linear(256, num_color)
+            nn.Linear(256, num_color),
         )
 
     def forward(self, x):
         x = self.base_model(x)
-        x = self.bn(F.relu(self.x1(x)))
+        x = F.relu(self.x1(x))
+        # out_type = self.y1(x)
+        # out_color = self.y2(x)
+
         out_type = self.y1(x)
         out_color = self.y2(x)
         return out_type, out_color
     def model_core(self):
-        model = models.resnet152(pretrained=True)
+        model = models.efficientnet_b0(pretrained=True)
+        for param in model.parameters():
+            param.requires_grad = False
+        return model
+class MultiOutputModel_3(nn.Module):
+    def __init__(self):
+        super(MultiOutputModel_3, self).__init__()
+        self.base_model = self.model_core()
+
+        self.y1 = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Conv2d(512, 1000, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+            nn.Flatten(),
+            nn.Linear(1000, num_type)
+            )
+        self.y2 = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Conv2d(512, 1000, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+            nn.Flatten(),
+            nn.Linear(1000, num_color)
+        )
+
+    def forward(self, x):
+        x = self.base_model(x)
+        out_type = self.y1(x)
+        out_color = self.y2(x)
+        return out_type, out_color
+    def model_core(self):
+        model = models.squeezenet1_1(pretrained=True).features
+        for param in model.parameters():
+            param.requires_grad = False
+        return model
+
+class MultiOutputModel_DenseNet(nn.Module):
+    def __init__(self):
+        super(MultiOutputModel_DenseNet, self).__init__()
+        self.base_model = self.model_core()
+
+        self.y1 = nn.Sequential(
+            nn.Linear(in_features=1000, out_features=num_type, bias=True)
+            )
+        self.y2 = nn.Sequential(
+            nn.Linear(in_features=1000, out_features=num_color, bias=True)
+        )
+
+    def forward(self, x):
+        x = self.base_model(x)
+        out_type = self.y1(x)
+        out_color = self.y2(x)
+        return out_type, out_color
+    def model_core(self):
+        model = models.densenet201(pretrained=True)
         for param in model.parameters():
             param.requires_grad = False
         return model
@@ -78,7 +132,125 @@ class MultiOutputModel_2(torch.nn.Module):
         model_ft.fc = nn.Linear(num_ftrs, 512)
         return model_ft
 
+class MobileNetV3_BackBone(nn.Module):
+    def __init__(self):
+        super(MobileNetV3_BackBone, self).__init__()
+
+        self.base_model = self.model_core()
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
+
+        self.y1 = nn.Sequential(
+            nn.Linear(in_features=576, out_features=1024, bias=True),
+            nn.Hardswish(),
+            nn.Dropout(p=0.2, inplace=True),
+            nn.Linear(in_features=1024, out_features=num_type, bias=True)
+        )
+        self.y2 = nn.Sequential(
+            nn.Linear(in_features=576, out_features=1024, bias=True),
+            nn.Hardswish(),
+            nn.Dropout(p=0.2, inplace=True),
+            nn.Linear(in_features=1024, out_features=num_color, bias=True)
+        )
+
+    def forward(self, x):
+        x = self.base_model(x)
+        x1 = self.avgpool(x)
+        x1 = x1.reshape(x1.size(0), -1)
+        out_type = self.y1(x1)
+        out_color = self.y2(x1)
+
+        return out_type, out_color
+    def model_core(self):
+        model_ft = models.mobilenet_v3_small(pretrained=True).features  # Choose your model backbone
+        for param in model_ft.parameters():
+            param.requires_grad = False
+        return model_ft
+
+class Resnet_BackBone(nn.Module):
+    def __init__(self):
+        super(Resnet_BackBone, self).__init__()
+
+        self.base_model = self.model_core()
+
+        self.y1 = nn.Sequential(
+            nn.Linear(in_features=512, out_features=num_type, bias=True),
+        )
+        self.y2 = nn.Sequential(
+            nn.Linear(in_features=512, out_features=num_color, bias=True),
+        )
+
+    def forward(self, x):
+        x = self.base_model(x)
+        out_type = self.y1(x)
+        out_color = self.y2(x)
+        return out_type, out_color
+    def model_core(self):
+        model_ft = models.resnet50(pretrained=True)  # Choose your model backbone
+        for param in model_ft.parameters():
+            param.requires_grad = False
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, 512)
+        return model_ft
+
+class SqueezeNet_BackBone(nn.Module):
+    def __init__(self):
+        super(SqueezeNet_BackBone, self).__init__()
+        self.base_model = self.model_core()
+
+        self.y1 = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Conv2d(512, 1000, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+            nn.Flatten(),
+            nn.Linear(1000, num_type)
+            )
+        self.y2 = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Conv2d(512, 1000, kernel_size=(1, 1), stride=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+            nn.Flatten(),
+            nn.Linear(1000, num_color)
+        )
+
+    def forward(self, x):
+        x = self.base_model(x)
+        out_type = self.y1(x)
+        out_color = self.y2(x)
+        return out_type, out_color
+    def model_core(self):
+        model = models.squeezenet1_1(pretrained=True).features
+        for param in model.parameters():
+            param.requires_grad = False
+        return model
+
+class Densenet_BackBone(nn.Module):
+    def __init__(self):
+        super(Densenet_BackBone, self).__init__()
+
+        self.base_model = self.model_core()
+
+        self.y1 = nn.Sequential(
+            nn.Linear(in_features=512, out_features=num_type, bias=True),
+        )
+        self.y2 = nn.Sequential(
+            nn.Linear(in_features=512, out_features=num_color, bias=True),
+        )
+
+    def forward(self, x):
+        x = self.base_model(x)
+        out_type = self.y1(x)
+        out_color = self.y2(x)
+        return out_type, out_color
+    def model_core(self):
+        model_ft = models.densenet161(pretrained=True)  # Choose your model backbone
+        for param in model_ft.parameters():
+            param.requires_grad = False
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, 512)
+        return model_ft
+
 if __name__ == '__main__':
-    model = models.resnet152(pretrained=True)
-    num_features = model.fc
+    model = models.densenet161()
     print(model)
--- train.py
+++ train.py
@@ -2,11 +2,11 @@ from lib import *
 from config import *
 from dataloader import get_dataloader_dict
 from dataset import MyDataset, make_data_path_list
-from model import MultiOutputModel, MultiOutputModel_2
+from model import *
 from utils import *
 from transforms import ImageTransform
 
-def train(model, dataloader_dict : dict, dataset_size : dict, criterion, optimzier, scheduler, num_epochs=num_epochs):
+def train(model, dataloader_dict : dict, dataset_size : dict, criterion, optimzier, scheduler, name_model, num_epochs=num_epochs):
     # Device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(device)
@@ -21,8 +21,8 @@ def train(model, dataloader_dict : dict, dataset_size : dict, criterion, optimzi
     best_acc_type = 0.0
     best_acc_color = 0.0
 
-    writer_loss = SummaryWriter(f'runs/Resnet152/Loss')
-    writer_acc = SummaryWriter(f'runs/Resnet152/Acc')
+    writer_loss = SummaryWriter(f'runs/{name_model}/Loss')
+    writer_acc = SummaryWriter(f'runs/{name_model}/Acc')
     step = 0
 
     for epoch in range(num_epochs):
@@ -58,10 +58,10 @@ def train(model, dataloader_dict : dict, dataset_size : dict, criterion, optimzi
 
                     loss0 = criterion[0](outputs[0], type_label)
                     loss1 = criterion[1](outputs[1], color_label)
+                    loss = loss0 + loss1
 
                     # Backward + optimizer
                     if phase == 'train':
-                        loss = loss0 + loss1
                         loss.backward()
                         optimzier.step()
 
@@ -88,7 +88,7 @@ def train(model, dataloader_dict : dict, dataset_size : dict, criterion, optimzi
                 # print(preds_color == color_label.data)
                 for i in range(batch_size):
                     running_correct += torch.sum((preds_type[i] == type_label[i].data) and (preds_color[i] == color_label[i].data))
-                print(running_correct)
+                # print(running_correct)
             if phase == 'train':
                 scheduler.step()
 
@@ -100,12 +100,12 @@ def train(model, dataloader_dict : dict, dataset_size : dict, criterion, optimzi
             str = '{} / Epoch {}/{} / Traffic_loss: {:.4f} / Color_loss: {:.4f} / Epoch_acc: {:.4f} / Traffic_acc: {:.4f} / Color_acc: {:.4f}\n'.format(
                 phase, epoch, num_epochs - 1, loss0, loss1, epoch_acc, type_acc, color_acc)
             if phase == 'train':
-                save_to_txt('ResNet152', 'train', 'train.txt', str)
+                save_to_txt(f'{name_model}', 'train', 'train.txt', str)
             else:
-                save_to_txt('ResNet152', 'val', 'val.txt', str)
+                save_to_txt(f'{name_model}', 'val', 'val.txt', str)
 
             print(
-                '\n[{}] / Total_loss: {:.4f} / Traffic_loss: {:.4f} / Color_loss {:.4f}'.format(phase, loss0 + loss1, loss0, loss1))
+                '\n[{}] / Total_loss: {:.4f} / Traffic_loss: {:.4f} / Color_loss {:.4f}'.format(phase, loss, loss0, loss1))
             print('\n[{}] / Epoch_acc: {:.4f} / Traffic_acc: {:.4f} / Color_acc: {:.4f} '.format(phase, epoch_acc, type_acc, color_acc))
 
             # Deep copy the model
@@ -113,22 +113,22 @@ def train(model, dataloader_dict : dict, dataset_size : dict, criterion, optimzi
                 print('\n--> Saving with total loss of {}'.format(epoch_loss), 'improved over previous {}'.format(best_loss))
                 best_loss = epoch_loss
                 best_model_loss_wts = copy.deepcopy(model.state_dict())
-                save_model(best_model_loss_wts, 'best_model_loss_resnet152.pth')
+                save_model(best_model_loss_wts, f'best_model_loss_{name_model}.pth')
             if phase == 'val' and type_acc > best_acc_type:
                 print('\n--> Saving with acc of type: {}'.format(type_acc), 'improved over previous {}'.format(best_acc_type))
                 best_acc_type = type_acc
                 best_model_type_acc_wts = copy.deepcopy(model.state_dict())
-                save_model(best_model_type_acc_wts, 'best_model_type_acc_resnet152.pth')
+                save_model(best_model_type_acc_wts, f'best_model_type_acc_{name_model}.pth')
             if phase == 'val' and color_acc > best_acc_color:
                 print('\n--> Saving with acc of color: {}'.format(color_acc), 'improved over previous {}'.format(best_acc_color))
                 best_acc_color = color_acc
                 best_model_color_acc_wts = copy.deepcopy(model.state_dict())
-                save_model(best_model_color_acc_wts, 'best_model_color_acc_resnet152.pth')
+                save_model(best_model_color_acc_wts, f'best_model_color_acc_{name_model}.pth')
             if phase == 'val' and epoch_acc > best_acc:
-                print('\n--> Saving with acc: {}'.format(color_acc), 'improved over previous {}'.format(best_acc))
+                print('\n--> Saving with acc: {}'.format(epoch_acc), 'improved over previous {}'.format(best_acc))
                 best_acc = epoch_acc
                 best_model_acc_wts = copy.deepcopy(model.state_dict())
-                save_model(best_model_acc_wts, 'best_model_acc_resnet152.pth')
+                save_model(best_model_acc_wts, f'best_model_acc_{name_model}.pth')
 
         print()
     time_elapsed = time.time() - since
@@ -156,7 +156,7 @@ if __name__ == '__main__':
     }
 
     # Model
-    model = MultiOutputModel()
+    model = Densenet_BackBone()
 
     # Loss function
     criterion = [nn.CrossEntropyLoss(), nn.CrossEntropyLoss()]
@@ -165,7 +165,7 @@ if __name__ == '__main__':
     optimizer = optim.Adam(
         [
             {"params": model.base_model.parameters()},
-            {"params": model.x1.parameters(), "lr": lr_last},
+            # {"params": model.avgpool.parameters(), "lr": lr_last},
             {"params": model.y1.parameters(), "lr": lr_last},
             {"params": model.y2.parameters(), "lr": lr_last},
         ],
@@ -174,5 +174,5 @@ if __name__ == '__main__':
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
     # Training
-    train(model, dataloader_dict, dataset_size, criterion, optimizer_ft, exp_lr_scheduler, num_epochs)
+    train(model, dataloader_dict, dataset_size, criterion, optimizer_ft, exp_lr_scheduler, 'Densenet_161', num_epochs)
 
